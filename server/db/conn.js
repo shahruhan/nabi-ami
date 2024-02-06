f3d848b3e683004b4c6aee09a285c25a623a186b
Shahrukh <65569455+shahruhan@users.noreply.github.com>
2024-02-06 19:17:10 +0500

    Update conn.js

--- server/db/conn.js
+++ server/db/conn.js
@@ -4,9 +4,9 @@ const mongoose = require("mongoose");
 
 // for atlas connection
 
-// const DB = 'mongodb+srv://shahruhan:ruhan0077@cluster0.ias3lfx.mongodb.net/airService?retryWrites=true&w=majority';
+const DB = 'mongodb+srv://shahruhan:ruhan0077@cluster0.ias3lfx.mongodb.net/airService?retryWrites=true&w=majority';
 
-const DB = process.env.DATABASE;
+// const DB = process.env.DATABASE;
 
 mongoose.connect(DB, { useUnifiedTopology: true, useNewUrlParser: true });
 const connection = mongoose.connection;
